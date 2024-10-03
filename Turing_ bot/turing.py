7ba648f732055edc5bad0fed0c78bdb509da0475
ALbertEO3 <marichalalberto292@gmail.com>
2024-10-03 01:03:24 -0400

    .

--- Turing_ bot/turing.py
+++ Turing_ bot/turing.py
@@ -3,10 +3,33 @@ import os
 import google.generativeai as genai
 from telebot.types import ReplyKeyboardMarkup
 
+
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN)
+
+
+def enviar_doc(asignatura, doc_pedido):
+    pass
+
+
+def buttons():
+    botones = ReplyKeyboardMarkup(
+        input_field_placeholder="Selecione la asignatura", resize_keyboard=True
+    )
+    botones.add(
+        "TC1",
+        "TC2",
+        "TC3",
+        "Mundiales",
+        "Extras",
+        "Ordinarios",
+        "Libros",
+        "Youtube",
+        "Volver",
+    )
+    return botones
 
 
 @bot.message_handler(commands=["start"])
@@ -17,51 +40,52 @@ def start(message):
     keyboard.add(
         "/Álgebra",
         "/Lógica",
-        "/Análisis Matemático 1",
-        "/Análisis Matematico 2",
-        "/Programación C#",
-        "/Programación Python",
+        "/AM1",
+        "/AM2",
+        "/C#",
+        "/python",
     )
     bot.reply_to(message, "bienvenidos al proyecto turing", reply_markup=keyboard)
 
 
-@bot.message_handler(commands=["Análisis Matemático 1"])
+@bot.message_handler(commands=["AM1"])
 def AM1(message):
-    keyboard_AM1 = ReplyKeyboardMarkup(
-        input_field_placeholder="Análisis Matemático 1", resize_keyboard=True
-    )
-    keyboard_AM1.add("libros")
+    bot.send_message(message.chat.id, "AM1", reply_markup=buttons())
 
 
-@bot.message_handler(commands=["Análisis Matemático 2"])
+@bot.message_handler(commands=["AM2"])
 def AM2(message):
-    pass
+    bot.send_message(message.chat.id, "AM2", reply_markup=buttons())
 
 
 @bot.message_handler(commands=["Álgebra"])
 def AL(message):
-    bot.send_message(message.chat.id, "hola bienvenido a Álgebra")
+    bot.send_message(
+        message.chat.id, "hola bienvenido a Álgebra", reply_markup=buttons()
+    )
 
 
 @bot.message_handler(commands=["Lógica"])
 def L(message):
-    pass
+    bot.send_message(message.chat.id, "Lógica", reply_markup=buttons())
 
 
-@bot.message_handler(commands=["Programación C#"])
+@bot.message_handler(commands=["C#"])
 def ProCsharp(message):
-    pass
+    bot.send_message(message.chat.id, "Programación_C#", reply_markup=buttons())
 
 
-@bot.message_handler(commands=["Programación Python"])
+@bot.message_handler(commands=["python"])
 def ProPython(message):
-    pass
+    bot.send_message(message.chat.id, "Programación_python", reply_markup=buttons())
 
 
 @bot.message_handler(content_types=["text"])
 def text(message):
+
     if message.text.startswith("/"):
         bot.send_message(message.chat.id, "Comando no disponible")
+
     else:
         response = model.generate_content(
             message.text,
