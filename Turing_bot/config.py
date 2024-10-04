62b657ee8452e404c617b995a2af99327a24c52f
0HH02 <cbambino2002@gmail.com>
2024-10-04 18:57:02 -0400

    add tokens

--- Turing_bot/config.py
+++ Turing_bot/config.py
@@ -2,8 +2,8 @@
 import os
 import streamlit as st
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-TOKEN = os.getenv("TOKEN")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyCpQ7M42a7fsZtqCoeYrZCJDVOn-9GexP0")
+TOKEN = os.getenv("TOKEN", "7872510777:AAHhL7S1vwznoop42anPipzKA2AMVy9NsWQ")
 
 if not GOOGLE_API_KEY or not TOKEN:
     raise ValueError(
